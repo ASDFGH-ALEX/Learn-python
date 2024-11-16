23069e434e67e4546f69d7ccefedf93986564ec8
ASDFGH-ALEX <alexsabu053@gmail.com>
2024-11-16 16:48:48 +0530

    Add files via upload

---
+++ Calculatorprogram.py
@@ -0,0 +1,12 @@
+'''
+Author:Alex
+Date:16-11-2024
+Description:Addition
+'''
+first_number=int(input("Enter the first Number: "))
+second_number=int(input("Enter the second Number: "))
+sum=int(input(first_number+second_number))
+print(sum)
+
+
+
