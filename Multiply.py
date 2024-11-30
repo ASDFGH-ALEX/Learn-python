77aa41b0dcba4c4b1a8ea98f4c3f16904b377347
ASDFGH-ALEX <alexsabu053@gmail.com>
2024-11-30 16:22:38 +0530

    Add files via upload

---
+++ Multiply.py
@@ -0,0 +1,6 @@
+def multiply_of_number(num):
+    total=1
+    for i in num:
+        total*=i
+    return total
+print("Multiply_of_number: ",multiply_of_number((8,2,3,-1,7)))
---
+++ sum.py
@@ -0,0 +1,7 @@
+def sum_of_numbers(list):
+     total=0
+     for i in list:
+         total=+i
+     return total
+print("Sum_of_numbers:",sum_of_numbers((8,2,3,0,7)))
+
